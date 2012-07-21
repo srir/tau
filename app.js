a90de9ab7cb059f02acbb3879bf1c249ac765836
Sri Raghavan <sraghavan@twitter.com>
2012-07-21 18:44:40 -0400

    mustache templating, views

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var application_root = __dirname,
   passport  = require('passport'),
   LocalStrategy = require('passport-local').Strategy,
   mongoose  = require('mongoose'),
-  mustache  = require('mustache'),
+  cons      = require('consolidate'),
   path      = require('path'),
   models    = require('./models')(mongoose);
 
@@ -23,7 +23,6 @@ passport.use(new LocalStrategy(
   }
 ));
 
-
 passport.serializeUser(function(user, done) {
   done(null, user._id);
 });
@@ -33,7 +32,6 @@ passport.deserializeUser(function(id, done) {
     done(err, user);
   });
 });
-
 
 app = express.createServer();
 mongoose.connect("mongodb://localhost/tau");
@@ -49,13 +47,17 @@ app.configure(function(){
   app.use(app.router);
   app.use(express.static(path.join(application_root, "public")));
   app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+  app.engine('html', cons.handlebars);
   app.set('views', path.join(application_root, "views"));
-  // app.set('view engine', 'jade')
+  app.set('view engine', 'html');
 });
 
 app.get('/', function(req, res) {
   models.User.find(function(err, users) {
-    return res.send(users);
+    res.render('index', {
+      title: "TAU HERPDERP",
+      users: users
+    })
   });
 });
 
@@ -63,10 +65,10 @@ app.get('/login', function(req, res){
   res.send("DUDE log in");
 //  res.render('login', { user: req.user, message: req.flash('error') });
 });
-app.post('/login', passport.authenticate('local',
-                                         { failureRedirect: '/login'}),
-         function (req, res) {
-             res.send("authenticated");
+
+app.post('/login', passport.authenticate('local', { failureRedirect: '/login'}),
+  function (req, res) {
+    res.send("authenticated");
 });
 
 
@@ -80,12 +82,12 @@ app.get('/logout', function(req, res){
   res.redirect('/');
 });
 
-console.log("Listening on port " + port);
-app.listen(port);
-
-
 //login decorators
 function ensureAuthenticated(req, res, next) {
   if (req.isAuthenticated()) { return next(); }
   res.redirect('/login')
 }
+
+app.listen(port, function() {
+  console.log("App listening on port %d in %s mode", 3000, app.settings.env);
+});
