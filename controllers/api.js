56d6db4637ce4765161e47848228cd262244d905
Matt McKay <penguin36@gmail.com>
2012-07-22 00:25:00 -0700

    Added the second api call.

--- controllers/api.js
+++ controllers/api.js
@@ -10,25 +10,41 @@ module.exports = function(app) {
       };
 
   app.get('/api/user/:userid/course/:courseid', //authUtils.ensureAuthenticated,
-          function(req, res) {
-            m.Course.findById(req.params.courseid)
-              .exec(function (err, doc) {
-                if(!err) {
-                  if(doc) {
-                      if(_.find(doc.staff,
-                             function(u) {return (u == req.params.userid);})) {
-                          res.send(ok({isStaff: true}));
-                      } else {
-                          res.send(ok({isStaff: false}));
-                      }
-                  } else {
-                      res.send(notok("Document not found."));
-                  }
-                } else {
-                    console.log(err);
-                }
-              });
-  });
+    function(req, res) {
+      m.Course.findById(req.params.courseid).exec(function (err, doc) {
+        if(!err) {
+          if(doc) {
+            if(_.find(doc.staff,
+              function(u) {return (u == req.params.userid);})) {
+                res.send(ok({isStaff: true}));
+              } else {
+                res.send(ok({isStaff: false}));
+              }
+          } else {
+            res.send(notok("Document not found."));
+          }
+        } else {
+          console.log(err);
+        }
+      });
+    });
+
+  app.get('/api/user/:userid/course', //authUtils.ensureAuthenticated,
+    function(req, res) {
+      m.Course.find({}).or([{staff: req.params.userid},
+                   {students: req.params.userid}]).exec(
+        function(err, courses){
+          if(!err){
+            if(courses){
+              res.send(ok({courses: courses}));
+            }else{
+              res.send(notok("Courses not found"));
+            }
+          }else{
+            console.log(err);
+          }
+        });
+    });
 
   app.get('/review', function(req, res) {
     res.render('review');
